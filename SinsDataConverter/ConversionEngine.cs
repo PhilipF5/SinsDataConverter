7d501aa159a73e3078d02f9749d32184431db6f2
Philip Fulgham <philfulgham@gmail.com>
2017-12-01 21:20:35 -0500

    feat(conversion): add conversion engine, work on ScriptBuilder

---
+++ SinsDataConverter/ConversionEngine.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Diagnostics;
+using System.Configuration;
+
+namespace SinsDataConverter
+{
+	static class ConversionEngine
+	{
+		private static ScriptBuilder _builder;
+		private static FileStream _fileStream;
+		private static List<ConversionJob> _jobs;
+		private static FileInfo _scriptFile;
+		private static DirectoryInfo _scriptsLocation = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+
+		public static bool EnableLogging
+		{
+			get
+			{
+				bool enabled;
+				if (bool.TryParse(ConfigurationManager.AppSettings["EnableLogging"], out enabled))
+				{
+					return enabled;
+				}
+				return false;
+			}
+		}
+
+		public static ReadOnlyCollection<ConversionJob> Queue
+		{
+			get
+			{
+				return _jobs.AsReadOnly();
+			}
+		}
+
+		public static void AddJob(ConversionJob job)
+		{
+			_jobs.Add(job);
+		}
+
+		public static void OpenScriptFile()
+		{
+			_scriptFile = new FileInfo(_scriptsLocation.FullName + "\\" + DateTime.Now.ToString("YYYY-MM-DD_HHmmss") + ".bat");
+			_fileStream = _scriptFile.OpenWrite();
+		}
+
+		public static void RemoveJob(ConversionJob job)
+		{
+			_jobs.Remove(job);
+		}
+
+		public static async Task Run()
+		{
+			_builder.AddJobs(_jobs);
+			_builder.Build(_fileStream);
+			_fileStream.Close();
+
+			Process.Start(_scriptFile.FullName);
+
+			if (!EnableLogging)
+			{
+				_scriptFile.Delete();
+			}
+		}
+
+		public static void StartNew()
+		{
+			_builder = new ScriptBuilder();
+			_jobs = new List<ConversionJob>();
+		}
+	}
+}
--- SinsDataConverter/ScriptBuilder.cs
+++ SinsDataConverter/ScriptBuilder.cs
@@ -3,17 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SinsDataConverter
 {
 	class ScriptBuilder
 	{
-		private List<ConversionJob> _jobs;
+		private List<ConversionJob> _jobs = new List<ConversionJob>();
 		private DateTime _timestamp = DateTime.UtcNow;
 
-		public void LoadJobs(IEnumerable<ConversionJob> jobs)
+		public void AddJobs(IEnumerable<ConversionJob> jobs)
 		{
+			_jobs.AddRange(jobs);
+		}
 
+		public Stream Build(Stream outputStream = null)
+		{
+			if (outputStream == null)
+			{
+				outputStream = new MemoryStream();
+			}
+			using (var writer = new StreamWriter(outputStream))
+			{
+				writer.WriteLine("@echo off");
+				foreach (var job in _jobs)
+				{
+					writer.WriteLine(job.ToString());
+				}
+			}
 		}
 	}
 }
